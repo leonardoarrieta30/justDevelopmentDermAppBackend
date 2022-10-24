85922a66373859800c3c3d7cce05e85d71f47809
Jasmin Pomasoncco <u20201b236@upc.edu.pe>
2022-10-24 18:41:16 -0500

    initial commit

---
+++ src/main/java/com/DermAppp/Backend/BackendDermAppApplication.java
@@ -0,0 +1,13 @@
+package com.DermAppp.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDermAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendDermAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/DermAppp/Backend/BackendDermAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DermAppp.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDermAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
