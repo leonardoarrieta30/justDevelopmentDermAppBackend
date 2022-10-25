b2cbfb26142c2a177d88a9932176992d8eac4e78
Jasmin Pomasoncco <89169379+jasminpomasoncco@users.noreply.github.com>
2022-10-25 00:18:21 -0500

    feat(dermatologists): added update dermatologist resource.

---
+++ src/main/java/com/DermAppp/Backend/overview/resource/UpdateDermatologistResource.java
@@ -0,0 +1,35 @@
+package com.DermAppp.Backend.overview.resource;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.*;
+
+@Getter
+@Setter
+public class UpdateDermatologistResource {
+
+     private Long id;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 60)
+    private String name;
+
+    @Max(70)
+    @Min(18)
+    private int age;
+
+    @Size(max = 60)
+    private String password;
+
+    @Size(max = 240)
+    private String address;
+
+    @Size(max = 400)
+    private String description;
+}
