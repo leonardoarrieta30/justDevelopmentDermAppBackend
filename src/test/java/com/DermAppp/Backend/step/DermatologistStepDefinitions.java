bb959dfcf044041147a4adc0e4ec9f9c715976fd
Arru <u202018777@upc.edu.pe>
2022-10-30 23:43:12 -0500

    feat(dermatologists): Added When Dermatologist Step Definitions.

--- src/test/java/com/DermAppp/Backend/step/DermatologistStepDefinitions.java
+++ src/test/java/com/DermAppp/Backend/step/DermatologistStepDefinitions.java
@@ -1,10 +1,15 @@
 package com.DermAppp.Backend.step;
 
+import com.DermAppp.Backend.overview.resource.CreateDermatologistResource;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
 @CucumberContextConfiguration
@@ -23,5 +28,19 @@ public class DermatologistStepDefinitions {
     @Given("The Endpoint {string} is available")
     public void theEndpointIsAvailable(String endpointPath) {
         this.endpointPath = String.format(endpointPath, randomServerPort);
+    }
+
+    @When("A post Request is sent with values {string}, {int}, {string}, {string}, {string}")
+    public void aPostRequestIsSentWithValues(String name, int age, String password, String address, String description) {
+        CreateDermatologistResource resource = new CreateDermatologistResource()
+                .withName(name)
+                .withAge(age)
+                .withPassword(password)
+                .withAddress(address)
+                .withDescription(description);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<CreateDermatologistResource> request = new HttpEntity<>(resource, headers);
+        responseEntity = testRestTemplate.postForEntity(endpointPath, request, String.class );
     }
 }
