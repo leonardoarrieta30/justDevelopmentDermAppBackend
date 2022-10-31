c20d0fec2820d36552552a48ef7da9fb745ae3fe
Arru <u202018777@upc.edu.pe>
2022-10-30 23:45:27 -0500

    feat(dermatologists): Added And Dermatologist Step Definitions.

--- src/test/java/com/DermAppp/Backend/step/DermatologistStepDefinitions.java
+++ src/test/java/com/DermAppp/Backend/step/DermatologistStepDefinitions.java
@@ -1,6 +1,10 @@
 package com.DermAppp.Backend.step;
 
 import com.DermAppp.Backend.overview.resource.CreateDermatologistResource;
+import com.DermAppp.Backend.overview.resource.DermatologistResource;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.cucumber.core.internal.com.fasterxml.jackson.core.JsonProcessingException;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -53,4 +57,24 @@ public class DermatologistStepDefinitions {
         assertThat(expectedStatusCode).isEqualTo(actualStatusCode);
     }
 
+    @And("A Dermatologist Resource is included in Response Body, with values {string}, {int}, {string}, {string}, {string}")
+    public void aDermatologistResourceIsIncludedInResponseBodyWithValues(String name, int age, String password, String address, String description) {
+        DermatologistResource expectedResource = new DermatologistResource()
+                .withName(name)
+                .withAge(age)
+                .withPassword(password)
+                .withAddress(address)
+                .withDescription(description);
+        String value = responseEntity.getBody();
+        ObjectMapper mapper = new ObjectMapper();
+        DermatologistResource actualResource;
+        try {
+            actualResource = mapper.readValue(value, DermatologistResource.class);
+
+        } catch (JsonProcessingException | NullPointerException e){
+            actualResource = new DermatologistResource();
+        }
+        expectedResource.setId(actualResource.getId());
+        assertThat(expectedResource).usingRecursiveComparison().isEqualTo(actualResource);
+    }
 }
