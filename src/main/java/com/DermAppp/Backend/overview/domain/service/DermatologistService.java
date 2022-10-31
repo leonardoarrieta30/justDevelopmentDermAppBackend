e9355cec679bacfd7118e41077a0de9c5248829d
Jasmin Pomasoncco <89169379+jasminpomasoncco@users.noreply.github.com>
2022-10-30 22:26:36 -0500

    feat(Service): Added Dermatologist Service.

---
+++ src/main/java/com/DermAppp/Backend/overview/domain/service/DermatologistService.java
@@ -0,0 +1,19 @@
+package com.DermAppp.Backend.overview.domain.service;
+
+import com.DermAppp.Backend.overview.domain.model.entity.Dermatologist;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface DermatologistService {
+    List<Dermatologist> getAll();
+    Page<Dermatologist> getAll(Pageable pageable);
+    Dermatologist getById(Long dermatologistId);
+    Dermatologist create(Dermatologist dermatologist);
+    Dermatologist update(Long dermatologistId, Dermatologist request);
+    ResponseEntity<?> delete(Long dermatologistId);
+
+
+}
