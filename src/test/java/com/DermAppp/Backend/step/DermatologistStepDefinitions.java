1dbc4594e37d2be1490135c3d1862b4d9eb99ba5
Arru <u202018777@upc.edu.pe>
2022-10-31 23:09:48 -0500

    fix: Updated Dermatologist Step Definitions.

--- src/test/java/com/DermAppp/Backend/step/DermatologistStepDefinitions.java
+++ src/test/java/com/DermAppp/Backend/step/DermatologistStepDefinitions.java
@@ -2,8 +2,9 @@ package com.DermAppp.Backend.step;
 
 import com.DermAppp.Backend.overview.resource.CreateDermatologistResource;
 import com.DermAppp.Backend.overview.resource.DermatologistResource;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.cucumber.core.internal.com.fasterxml.jackson.core.JsonProcessingException;
+
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
