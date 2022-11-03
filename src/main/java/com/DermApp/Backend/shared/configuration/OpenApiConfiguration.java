ab0beb97bb0d8e0afd3ce77b5e637d0bde8e9a01
Thomas-mh <thomas.dumont@edu.ece.fr>
2022-11-03 12:08:10 -0500

    doc: added configuration for documentation.

---
+++ src/main/java/com/DermApp/Backend/shared/configuration/OpenApiConfiguration.java
@@ -0,0 +1,35 @@
+package com.DermApp.Backend.shared.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+
+public class OpenApiConfiguration {
+
+    @Bean
+    public OpenAPI customOpenAPi(
+            @Value("${documentation.application.description}")
+            String applicationDescription,
+            @Value("${documentation.application.version}")
+            String applicationVersion
+    ){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("DermApp API")
+                        .version(applicationVersion)
+                        .description(applicationDescription)
+                        .termsOfService("https://dermApp.com/tos")
+                        .license(new License()
+                                .name("Apache 2.0 License")
+                                .url("https://dermApp.com/license"))
+                        .contact(new Contact()
+                                .url("https://dermApp.com")
+                                .name("Derm,.App")));
+    }
+}
