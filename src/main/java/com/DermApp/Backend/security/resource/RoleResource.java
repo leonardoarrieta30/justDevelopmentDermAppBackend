c37a3a0da4f1f3ba2a389846b4ed482174e64fdd
Arru <u202018777@upc.edu.pe>
2022-11-08 00:44:18 -0500

    feat(security): Added request/response register types.

---
+++ src/main/java/com/DermApp/Backend/security/resource/RoleResource.java
@@ -0,0 +1,14 @@
+package com.DermApp.Backend.security.resource;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@With
+public class RoleResource {
+
+    private Long id;
+    private String name;
+}
